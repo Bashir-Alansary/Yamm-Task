e7c93e9161597a93f30b6036a5252885fa750773
Bashir Alansary <bashiralansary306@gmail.com>
2025-02-15 16:29:40 +0200

    edit the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
-import { IoSearch } from "react-icons/io5";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { FaCog } from "react-icons/fa";
+import Image from 'next/image';
 
 const Header = () => {
   return (
-    <div className='h-20 bg-blue-500 mb-8'>
+    <div className='flex justify-between items-center mb-8'>
         <form>
-            <div className='bg-white h-12 flex'>
-                <span className='h-full flex items-center px-5'><IoSearch /></span>
-                <input type='text' placeholder='search something' className='h-full'/>
+            <div className='w-fit h-12'>
+                <input type='text' placeholder='search something' className='h-full w-96 px-7 rounded-md bg-white shadow-md'/>
             </div>
         </form>
+        <div className='flex items-center'>
+          <button className='size-14 text-xl bg-white flex items-center justify-center shadow-md rounded-md'>
+            <IoIosNotificationsOutline />
+          </button>
+          <button className='size-14 text-xl bg-white flex items-center justify-center shadow-md rounded-md ml-4'>
+            <FaCog />
+          </button>
+          <Image
+            src="/images/user.jpg"
+            width={64}
+            height={64}
+            alt="profile"
+            className=' ml-9 bg-white p-2 rounded-full shadow-md'
+          />
+        </div>
     </div>
   )
 }
